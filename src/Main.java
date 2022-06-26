fdde6e1ae4bed0bca360f4c56a685b804adef9af
Victor <knightivanhoe1194@gmail.com>
2022-06-26 11:21:56 +0300

    the first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20_505;
+        int gift = 20;
+        int bonus = amount / gift;
+
+        System.out.println("Количество бонусных миль: " + bonus);
+
+    }
+}
